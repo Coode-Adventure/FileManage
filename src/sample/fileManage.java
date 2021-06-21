129daf72d32c8cbaa2cc44754aadb83136c75905
SULAPIS <g2359620463@gmail.com>
2021-06-21 08:03:56 +0800

    测试

--- src/sample/fileManage.java
+++ src/sample/fileManage.java
@@ -5,17 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
-public class fileManage extends Application{
+public class fileManage extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Hello World");
 //        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, 600, 400));
+
+        Scene scene = new Scene(root, 600, 400);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("sample.css")).toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+
         primaryStage.show();
     }
 }
--- src/sample/sampleController.java
+++ src/sample/sampleController.java
@@ -1,14 +1,20 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 
+import java.io.*;
 import java.sql.*;
 
-public class sampleController {
+public class sampleController extends Node {
     @FXML
-    public Label l1;
+    public Button B1;
+    public TextArea T1;
+    public Button CreateButton;
+    public Button SelectButton;
 
     @FXML
     public void Bclick(MouseEvent event) {
@@ -27,14 +33,16 @@ public class sampleController {
             con = DriverManager.getConnection(url, user, password);
             if (!con.isClosed()) {
                 System.out.println("数据库连接成功");
+
+                Statement stmt = con.createStatement();
+                String sql = "select * from book";
+                ResultSet rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    System.out.println(rs.getString("bookname"));
+                    T1.setText(rs.getString("bookname"));
+                }
             }
-            Statement stmt = con.createStatement();
-            String sql = "select * from book";
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                System.out.println(rs.getString("bookname"));
-                l1.setText(rs.getString("bookname"));
-            }
+
             con.close();
         } catch (ClassNotFoundException e) {
             System.out.println("数据库驱动没有安装");
@@ -44,9 +52,36 @@ public class sampleController {
             System.out.println("数据库连接失败");
         }
 
-//        String a = "poi";
-//        String b = a + "p0";
-//        b = b + a;
-//        l1.setText(b + a);
+    }
+
+    public void CreateFile(MouseEvent event) throws IOException {
+        File f1 = new File("src/sample/file/test.txt");
+        if (f1.exists()) {
+            System.out.println("文件已存在");
+            try (BufferedReader bis = new BufferedReader(new FileReader("src/sample/file/test.txt"))){
+
+                StringBuilder data= new StringBuilder();
+                while (bis.ready()) {
+                    data.append(bis.readLine()+"\n");
+
+                }
+                T1.setText(String.valueOf(data));
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                f1.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("创建文件成功");
+        }
+
+
+    }
+
+    public void SelectFile(MouseEvent event) {
     }
 }
